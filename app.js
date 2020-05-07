b37ab5a6c8d7e97c72f919dc00d189c76cd0da0a
Ryan Roat <ryanroat@gmail.com>
2020-05-07 18:05:33 -0400

    configure server port, start server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('server');
+const express = require('express');
+
+const app = express();
+
+const localPort = 5001;
+
+app.listen(process.env.PORT || localPort);
+console.log(`Server started on port ${localPort}.`);
