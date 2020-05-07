4c72a24bb3db7a49eba7aaa1f5d12c4e015f32de
Ryan Roat <ryanroat@gmail.com>
2020-05-07 18:13:46 -0400

    render an index view

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const localPort = 5001;
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(process.env.PORT || localPort);
-console.log(`Server started on port ${localPort}.`);
+console.log(`Server started on port ${localPort}.`); // TODO remove for production
